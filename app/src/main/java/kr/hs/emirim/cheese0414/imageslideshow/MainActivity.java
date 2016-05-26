435b81b0b854ce6339ac7c233e3d04477d796835
Myoungok Kim <cheese0414@naver.com>
2016-05-26 20:14:17 +0900

    #2 Event Programming complete

--- app/src/main/java/kr/hs/emirim/cheese0414/imageslideshow/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/cheese0414/imageslideshow/MainActivity.java
@@ -1,13 +1,38 @@
 package kr.hs.emirim.cheese0414.imageslideshow;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ViewFlipper flip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        flip=(ViewFlipper)findViewById(R.id.view_flip);
+        flip.setFlipInterval(1000);
+        Button butStart=(Button)findViewById(R.id.but_start);
+        Button butStop=(Button)findViewById(R.id.but_stop);
+        butStart.setOnClickListener(this);
+        butStop.setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.but_start:
+                flip.startFlipping();
+                break;
+            case R.id.but_stop:
+                flip.stopFlipping();
+                break;
+        }
     }
 }
